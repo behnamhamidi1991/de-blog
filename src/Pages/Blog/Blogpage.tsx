d468e11186fb2d511c2cb9e7f98f293f55c37de7
aminiann <aminiann@uni-mainz.de>
2025-04-20 22:02:52 +0200

    new changes

---
+++ src/Pages/Blog/Blogpage.tsx
@@ -0,0 +1,11 @@
+function Blogpage() {
+  return (
+    <div>
+      <div>
+        <h1>blog page </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Blogpage;
---
+++ src/Pages/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <div>
+        <h1>home page </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
